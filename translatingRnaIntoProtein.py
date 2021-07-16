a755159cfce7caa73510e2405dbea29a4a44a37e
ZIONKIM <rlswldhs@naver.com>
2021-07-16 22:39:38 +0900

    0716

---
+++ translatingRnaIntoProtein.py
@@ -0,0 +1,80 @@
+# "STOP" -> ""
+d_rnaCodonTable = {
+    "UUU": "F",
+    "CUU": "L",
+    "AUU": "I",
+    "GUU": "V",
+    "UUC": "F",
+    "CUC": "L",
+    "AUC": "I",
+    "GUC": "V",
+    "UUA": "L",
+    "CUA": "L",
+    "AUA": "I",
+    "GUA": "V",
+    "UUG": "L",
+    "CUG": "L",
+    "AUG": "M",
+    "GUG": "V",
+    "UCU": "S",
+    "CCU": "P",
+    "ACU": "T",
+    "GCU": "A",
+    "UCC": "S",
+    "CCC": "P",
+    "ACC": "T",
+    "GCC": "A",
+    "UCA": "S",
+    "CCA": "P",
+    "ACA": "T",
+    "GCA": "A",
+    "UCG": "S",
+    "CCG": "P",
+    "ACG": "T",
+    "GCG": "A",
+    "UAU": "Y",
+    "CAU": "H",
+    "AAU": "N",
+    "GAU": "D",
+    "UAC": "Y",
+    "CAC": "H",
+    "AAC": "N",
+    "GAC": "D",
+    "UAA": "",
+    "CAA": "Q",
+    "AAA": "K",
+    "GAA": "E",
+    "UAG": "",
+    "CAG": "Q",
+    "AAG": "K",
+    "GAG": "E",
+    "UGU": "C",
+    "CGU": "R",
+    "AGU": "S",
+    "GGU": "G",
+    "UGC": "C",
+    "CGC": "R",
+    "AGC": "S",
+    "GGC": "G",
+    "UGA": "",
+    "CGA": "R",
+    "AGA": "R",
+    "GGA": "G",
+    "UGG": "W",
+    "CGG": "R",
+    "AGG": "R",
+    "GGG": "G",
+}
+
+fr = open("rosalind_prot.txt", "r")
+ls = []
+s = ""
+for line in fr:
+    line.strip()
+    ls.append(line)
+s = "".join(line)
+
+for i in range(int(len(s) / 3)):
+    print(d_rnaCodonTable[s[i * 3 : i * 3 + 3]], end="")
+
+fr.close()
